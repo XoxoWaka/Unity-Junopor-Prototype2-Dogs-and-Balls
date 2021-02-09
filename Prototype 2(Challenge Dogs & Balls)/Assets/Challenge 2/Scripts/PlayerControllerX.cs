d52011608614a654e06c87dc25ec1076f8c463be
Alex Kurochkin <pin-pong-pin@mail.ru>
2021-02-09 20:22:51 +0300

    init commit

---
+++ Prototype 2(Challenge Dogs & Balls)/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public bool canTrowDog = true; // переменная которая не дает нам выпускать собак и будет переключаться
+                                   // своим методом
+
+    private void Start()
+    {
+        InvokeRepeating("CanTrowDogAgain", 2, 2); //вызываем свой метод который переключает canTrowDog на тру и
+                                                  // мы можем выпускать собаку
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canTrowDog)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canTrowDog = false; // при выпуске собаки мы делаем выпуск собаки невозможным следующий
+                                // выпуск будет доступен после вызова InvokeReapiting;
+        }
+    }
+
+    void CanTrowDogAgain()
+    {
+        canTrowDog = true;
+    }
+}
